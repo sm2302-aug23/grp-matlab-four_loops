b8c5ad04b26dda7239469bb599f02e6ed47c5a23
Ak Afdieka Hazimie <141107111+fdkahzme@users.noreply.github.com>
2023-11-07 11:43:35 +0800

    names.m

--- names.m
+++ names.m
@@ -82,3 +82,29 @@ xlabel('x')
 ylabel('y')
 set (gca, 'Fontsize', 10, 'LineWidth', 1)
 grid on
+
+% Data name for afdieka
+x = [0 0.3 0.6 1 1.1 0.2 1.2 1.3 1.5 2 1.5 2.5];
+y = [0 2 2.9 2 0 1.2 1.4 3 0.5 2 1.4 0];
+
+n = length(x);
+t = 0:n-1; % Parametric coordinate t
+tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
+
+% Compute spline interpolation
+ xx = spline(t, x, tt);
+ yy = spline(t, y, tt);
+
+ % Plot settings
+ figure(1)
+ subplot(2,2,1);
+ plot(xx, yy, 'k', 'LineWidth', 2) % plot spline
+ hold on
+ plot(x, y, 'bo', 'MarkerFaceColor', 'b') % plot data points
+ axis([-0.2 6 -0.2 4.5])
+ grid on
+ title('Spline Interpolation of Cursive Name ak')
+ xlabel('x')
+ ylabel('y')
+ set(gca, 'FontSize', 10, 'LineWidth', 1)
+ grid on
